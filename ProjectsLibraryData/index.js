48074072a58852acae501d2898cf8870128d53b2
strigonone <s15jaychao@hotmail.com>
2021-04-17 01:15:14 +1200

    Updated backend / API

--- ProjectsLibraryData/index.js
+++ ProjectsLibraryData/index.js
@@ -1,6 +1,11 @@
 // get the client
 const mysql = require("mysql2");
 const fs = require("fs");
+const cors = require("cors");
+const express = require("express");
+
+const app = express();
+app.use(cors());
 
 // create the connection to database
 const connection = mysql.createConnection({
@@ -15,9 +20,38 @@ const connection = mysql.createConnection({
 	},
 });
 
+const parseResultToJSON = (resultObj) => JSON.parse(JSON.stringify(resultObj));
+
 // simple query
-connection.query("SELECT * FROM `Project_Tables`", (err, results, fields) => {
-	console.log(results); // results contains rows returned by server
-	console.log(fields); // fields contains extra meta data about results, if available
-	console.log(err);
+// connection.query("SELECT * FROM `Project_Tables`", (err, results, fields) => {
+// 	console.log(results); // results contains rows returned by server
+// 	// console.log(fields); // fields contains extra meta data about results, if available
+// 	const jsonResults = parseResultToJSON(results);
+// 	console.log(jsonResults);
+// 	// console.log(err);
+// });
+
+const runDBQuery = () => {
+	const yourQuery = "SELECT * FROM `Project_Tables`";
+	return connection.promise().query(yourQuery);
+};
+
+// When a API request is made to localhost:3000/TeacherProjectsPage, the DB query is executed
+app.get("/teamzeromissionreadybackend", (req, res) => {
+	runDBQuery()
+		.then((queryResult, fields) => {
+			const [rows] = queryResult;
+			const jsonResults = parseResultToJSON(rows);
+			console.log("Sending response for GET", jsonResults);
+			// Sends the response if query was successful.
+			res.send(jsonResults);
+		})
+		.catch((error) => {
+			console.log(error);
+			// Sends an error if the query returned an error.
+			res.status(500).send(error);
+		});
 });
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Server ready at port ${port}`));
